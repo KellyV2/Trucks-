166398fb21107d60debaa5c7e8b39295c0b2d61d
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-09-09 05:48:43 +0000

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,5 +1,11 @@
 #include "Truck.h"
+#include <iostream>
 
 int main() {
+    Truck truck_1(100, "yes");
 
+    cout << truck_1.get_brand_name() << endl;
+    cout << truck_1.get_brand_code() << endl;
+
+    return 0;
 }
